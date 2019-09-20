e51d51553aee2305f2f1ae1a7cad7684b6065f3a
elizanikol <55412039+elizanikol@users.noreply.github.com>
2019-09-20 23:35:19 +0300

    Update Default.java

--- lab1/src/Default.java
+++ lab1/src/Default.java
@@ -1,25 +1,26 @@
 public class Default extends Mode {
     
+    private String[] fileNames_;
+    private String hashType_;
+    
     public Default(String[] args) {
         this.fileNames_ = java.util.Arrays.copyOfRange(args, 2, args.length);
-        this.hashType_= args[0];
+        this.hashType_ = args[0];
     }
 
     @Override
     public void calcHash() {
         
-        if (hashType_.equals("-md5"))     
+        if (hashType_.equals("-md5")) {    
             for (String fileName : fileNames_) {
                 System.out.println("For file: " + fileName);
                 md5Hash(fileName);
             } 
-        else
+        } else {
             for (String fileName : fileNames_) {
                 System.out.println("For file: " + fileName);
                 sha256Hash(fileName);
             }
+        }
     }
-
-    private String[] fileNames_;
-    private String hashType_;
 }
