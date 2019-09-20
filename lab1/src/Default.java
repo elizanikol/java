2bfd7fe5602d818b80f33d1827dcc334680ce13f
elizanikol <55412039+elizanikol@users.noreply.github.com>
2019-09-20 19:45:32 +0300

    Create Default.java

---
+++ lab1/src/Default.java
@@ -0,0 +1,25 @@
+public class Default extends Mode {
+    
+    public Default(String[] args) {
+        this.fileNames_ = java.util.Arrays.copyOfRange(args, 2, args.length);
+        this.hashType_= args[0];
+    }
+
+    @Override
+    public void calcHash() {
+        
+        if (hashType_.equals("-md5"))     
+            for (String fileName : fileNames_) {
+                System.out.println("For file: " + fileName);
+                md5Hash(fileName);
+            } 
+        else
+            for (String fileName : fileNames_) {
+                System.out.println("For file: " + fileName);
+                sha256Hash(fileName);
+            }
+    }
+
+    private String[] fileNames_;
+    private String hashType_;
+}
