a36486faab95becd7ccfb771a25ed7ea03a7871c
elizanikol <55412039+elizanikol@users.noreply.github.com>
2019-09-20 19:44:37 +0300

    Create Main.java

---
+++ lab1/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        
+        if(args.length < 2)
+            throw new Exception("Invalid input format: not enough arguments");
+
+        String hashType = args[0];
+
+        switch(hashType) {            
+            case "-i":
+                if (args.length != 2)
+                    throw new Exception("Invalid input format: not enough arguments");
+                Interactive interMode = new Interactive(args[1]);
+                interMode.calcHash();
+                interMode.menu();           
+                break;
+
+            case "-md5": case "-sha256":
+                if (args.length < 3)
+                    throw new Exception("Invalid input format: not enough arguments");
+                if (!args[1].equals("-f"))
+                    throw new Exception("Invalid flag");
+                Default defMode = new Default(args);
+                defMode.calcHash();
+                break;
+
+            default: System.out.println("Invalid hash type");
+        }
+    }
+}
