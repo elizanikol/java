249435f3c0f0e19a6bd665aecbde5bb3eee75a2c
elizanikol <55412039+elizanikol@users.noreply.github.com>
2019-09-20 23:28:52 +0300

    Update Main.java

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -5,25 +5,30 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         
-        if(args.length < 2)
+        if (args.length < 2) {
             throw new Exception("Invalid input format: not enough arguments");
-
+        }
+        
         String flag = args[0];
 
         switch(flag) {            
             case "-i":
-                if (args.length != 2)
+                if (args.length != 2) {
                     throw new Exception("Invalid input format: not enough arguments");
+                }
                 Interactive interMode = new Interactive(args[1]);
                 interMode.calcHash();
                 interMode.menu();           
                 break;
 
-            case "-md5": case "-sha256":
-                if (args.length < 3)
+            case "-md5": 
+            case "-sha256":
+                if (args.length < 3) {
                     throw new Exception("Invalid input format: not enough arguments");
-                if (!args[1].equals("-f"))
+                }
+                if (!args[1].equals("-f")) {
                     throw new Exception("Invalid flag");
+                }
                 Default defMode = new Default(args);
                 defMode.calcHash();
                 break;
